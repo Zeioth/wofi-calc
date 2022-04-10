5b3dacd7e5924926ae6bf4b71011c0a8c2400b5d
Zeioth <zeioth@hotmail.com>
2022-04-10 23:56:40 +0200

    Now we can generate releases for wofi-calc-git and wofi-calc from the same repo.

--- aur-scripts/wofi-calc-git/push-version.sh
+++ aur-scripts/wofi-calc-git/push-version.sh
@@ -1,3 +1,5 @@
+# Description: Generates a new aur release.
+
 # Generate the checksums automatically
 sed -i "s/^sha256sums=.*/$(makepkg -g -f -p PKGBUILD)/" ./PKGBUILD
 
@@ -7,7 +9,7 @@ makepkg --printsrcinfo > .SRCINFO
 # Push it to AUR
 git add PKGBUILD .SRCINFO
 git commit -m "New version"
-git push ssh://aur@aur.archlinux.org/wofi-calc.git master
+git push ssh://aur@aur.archlinux.org/wofi-calc-git.git master
 
 # Bye
 echo -e "\n-------------------------------------------------------------------"
