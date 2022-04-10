6b8b47cdc91c0574151a0009464c5adf3ae9bcde
Zeioth <zeioth@hotmail.com>
2022-04-11 01:39:08 +0200

    Confirmed, AUR daily quota exceeded.
    the command  git clone ssh://aur.archlinux.org/wofi-calc-git.git
    return 'fatal: could no read from repository. So most likely the daily
    quota was exceeded, as no permission file was altered.'

--- .aur-scripts/wofi-calc/push-version.sh
+++ .aur-scripts/wofi-calc/push-version.sh
@@ -2,8 +2,17 @@
 # WARNING: make sure you push all changes before running this.
 #          or you may experience inconsistences.
 
+#-----------------------------------------------------------------------------
+# TODO: On github/gitlab, create automation to create git version per tag
+#       This is just in case an user tries to download an specific version.
+#-----------------------------------------------------------------------------
+
+
 # Generate the checksums automatically
 sed -i "s/^sha256sums=.*/$(makepkg -g -f -p PKGBUILD)/" ./PKGBUILD
+
+# Generate tag a new tag for the new version
+git tag $(printf "v1.0.r%s" "$(git rev-list --count HEAD)")
 
 # Generate pagkage metadata
 makepkg --printsrcinfo > ./.SRCINFO
@@ -11,7 +20,7 @@ makepkg --printsrcinfo > ./.SRCINFO
 # Push it to AUR
 git add ./PKGBUILD ./.SRCINFO
 git commit -m "New version"
-git push ssh://aur@aur.archlinux.org/wofi-calc-git.git master
+git push ssh://aur@aur.archlinux.org/wofi-calc.git master
 
 # Bye
 echo -e "\n-------------------------------------------------------------------"
