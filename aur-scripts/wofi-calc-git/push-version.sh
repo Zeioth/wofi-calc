90f5aa09af345a432b8b340d64c9f0a0404a1491
Zeioth <zeioth@hotmail.com>
2022-04-11 00:16:01 +0200

    test

--- aur-scripts/wofi-calc-git/push-version.sh
+++ aur-scripts/wofi-calc-git/push-version.sh
@@ -1,13 +1,13 @@
 # Description: Generates a new aur release.
 
 # Generate the checksums automatically
-sed -i "s/^sha256sums=.*/$(makepkg -g -f -p PKGBUILD)/" ./PKGBUILD
+sed -i "s/^sha256sums=.*/$(makepkg -g -f -p PKGBUILD)/" PKGBUILD
 
 # Generate pagkage metadata
 makepkg --printsrcinfo > .SRCINFO
 
 # Push it to AUR
-git add ./PKGBUILD ./.SRCINFO
+git add PKGBUILD .SRCINFO
 git commit -m "New version"
 git push ssh://aur@aur.archlinux.org/wofi-calc-git.git master
 
